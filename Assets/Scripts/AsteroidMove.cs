d4de0c67e24b8dacab87a67c0a0a82e1da530ff1
Jeren <jeren.mckey@sjsu.edu>
2019-03-20 19:17:41 -0700

    Added assets to prototype

--- Assets/Scripts/AsteroidMove.cs
+++ Assets/Scripts/AsteroidMove.cs
@@ -2,25 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AsteroidMove : MonoBehaviour {
+public class AsteroidMove : MonoBehaviour
+{
 
     private float speed;
     private Vector3 movementVector;
     private Vector2 posVector;
     private float startTime;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         startTime = Time.time;
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         if (Time.time - startTime >= 5) deleteAsteroid();
         transform.position += movementVector * Time.deltaTime;
-	}
+    }
 
     void startMove(float speed)
     {
---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour {
+    private int lives = 3;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (lives == 0)
+        {
+            Debug.Log("Game Ended");
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.tag != "Wall")
+        {
+            lives--;
+            Debug.Log(lives);
+        }
+    }
+}
